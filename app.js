4135b45583145990d2c2a5698e91eefdd2d4f204
DESKTOP-QQ71JCR\cDots1532 <s.dimitrov@cdots.bg>
2016-10-06 16:45:23 +0300

    small fix favicon

--- app.js
+++ app.js
@@ -24,7 +24,12 @@ app.use(function(req,res,next){
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "origin, content-type, accept");
 	next()
-})
+});
+
+app.get('/favicon.ico', function(req, res) {
+    res.send(200);
+});
+
 var indexEndpoint = require("./routes/indexEndpoint");
 app.use("/", indexEndpoint);
 
